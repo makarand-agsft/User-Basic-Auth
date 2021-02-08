a02ae996d47194d8e40f60e2080d3aa691b216f1
makar <'dmakarand@agsft.com'>
2021-02-08 23:45:39 +0530

    Added Jwt security config and email configs in properties

--- src/main/java/com/user/auth/jwtSecurity/JwtProvider.java
+++ src/main/java/com/user/auth/jwtSecurity/JwtProvider.java
@@ -17,61 +17,60 @@ import java.util.Map;
 @Component
 public class JwtProvider {
 
-    @Value("${jwt.secret}") public String secretKey;
+	@Value("${jwt.secret}")
+	public String secretKey;
 
-    @Value("${jwt.tokenValidity}") public Long jwtTokenValidity;
+	@Value("${jwt.tokenValidity}")
+	public Long jwtTokenValidity;
 
-    private Boolean isTokenExpired(String token) {
-        final Date expiration = getExpirationDateFromToken(token);
-        return expiration.before(new Date());
-    }
+	private Boolean isTokenExpired(String token) {
+		final Date expiration = getExpirationDateFromToken(token);
+		return expiration.before(new Date());
+	}
 
-    public String generateToken(User user) {
+	public String generateToken(User user) {
 
-        return doGenerateToken(user.getEmail());
-    }
+		Map<String, Object> claims = new HashMap<>();
+		claims.put("roles", user.getRoles());
+		claims.put("userName", user.getEmail());
+		claims.put("firstName", user.getFirstName());
+		claims.put("lastName", user.getLastName());
+		return Jwts.builder().setClaims(claims).setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + jwtTokenValidity * 1000))
+				.signWith(SignatureAlgorithm.HS256, secretKey).compact();
+	}
 
-    private String doGenerateToken(String subject) {
+	public Date getExpirationDateFromToken(String token) {
+		Date expiration;
+		try {
+			final Claims claims = getClaimFromToken(token);
+			expiration = claims.getExpiration();
+		} catch (Exception e) {
+			expiration = null;
+		}
+		return expiration;
+	}
 
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("roles", Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")));
-        claims.put("username", subject);
-        return Jwts.builder().setClaims(claims).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + jwtTokenValidity * 1000)).signWith(SignatureAlgorithm.HS256, secretKey)
-                .compact();
-    }
+	public Claims getClaimFromToken(String token) {
+		return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
 
-    public Date getExpirationDateFromToken(String token) {
-        Date expiration;
-        try {
-            final Claims claims = getClaimFromToken(token);
-            expiration = claims.getExpiration();
-        } catch (Exception e) {
-            expiration = null;
-        }
-        return expiration;
-    }
+	}
 
-    public Claims getClaimFromToken(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String username = getUsernameFromToken(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
 
-    }
+	public String getUsernameFromToken(String token) {
 
-    public Boolean validateToken(String token, UserDetails userDetails) {
-        final String username = getUsernameFromToken(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-    }
+		String username;
+		try {
+			final Claims claims = getClaimFromToken(token);
+			username = (String) claims.get("username");
+		} catch (Exception e) {
+			username = null;
+		}
+		return username;
 
-    public String getUsernameFromToken(String token) {
-
-        String username;
-        try {
-            final Claims claims = getClaimFromToken(token);
-            username = (String) claims.get("username");
-        } catch (Exception e) {
-            username = null;
-        }
-        return username;
-
-    }
+	}
 }

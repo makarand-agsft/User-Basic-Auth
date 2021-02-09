4769bd0116b36ed275417a6f69bc02043f8c82be
Makarand Deshpande <dmakarand@agsft.com>
2021-02-09 20:19:02 +0530

    added global auth header , added patches

---
+++ src/main/resources/db/patch2.sql
@@ -0,0 +1,10 @@
+use user_auth;
+set foreign_key_checks=0;
+set sql_safe_updates=0;
+
+insert into patch_version(patch_no,depend_patch,is_success) values(1,1,0);
+ALTER TABLE `user_auth`.`token`
+CHANGE COLUMN `token` `token` LONGTEXT NULL DEFAULT NULL ;
+insert into patch_version(patch_no,depend_patch,is_success) values(1,1,1);
+set foreign_key_checks=1;
+set sql_safe_updates=1;

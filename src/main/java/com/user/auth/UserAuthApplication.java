9b2581e8eb23f3be84b1947c9f757bd4c0fa2479
Makarand Deshpande <dmakarand@agsft.com>
2021-02-09 12:10:21 +0530

    added bean of modelmapper

--- src/main/java/com/user/auth/UserAuthApplication.java
+++ src/main/java/com/user/auth/UserAuthApplication.java
@@ -1,8 +1,10 @@
 package com.user.auth;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -13,4 +15,8 @@ public class UserAuthApplication {
 		SpringApplication.run(UserAuthApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }

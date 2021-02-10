a3ddaa0c3ca91cf332c719e6a32ea51af29d71d0
Makarand Deshpande <dmakarand@agsft.com>
2021-02-10 18:01:33 +0530

    added db script of bootstrap user

--- src/main/resources/db/TRUNCATE.sql
+++ src/main/resources/db/TRUNCATE.sql
@@ -3,6 +3,8 @@ set foreign_key_checks=0;
 set sql_safe_updates=0;
 truncate table user;
 truncate table  user_roles;
+truncate table user_profile;
+truncate address;
 truncate table token;
 set foreign_key_checks=1;
 set sql_safe_updates=1;

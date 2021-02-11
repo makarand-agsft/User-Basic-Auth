40b4c784752a5c04f93d0838ae3a3adf188ac4aa
Makarand Deshpande <dmakarand@agsft.com>
2021-02-11 11:49:53 +0530

    updated insert sql , changed is_deleted flag to boolean

--- src/main/resources/db/patch3.sql
+++ src/main/resources/db/patch3.sql
@@ -4,7 +4,7 @@ set sql_safe_updates=0;
 
 insert into patch_version(patch_no,depend_patch,is_success) values(3,2,0);
 ALTER TABLE `user_auth`.`user`
-ADD COLUMN `is_deleted` VARCHAR(45) NULL AFTER `password`;
+ADD COLUMN `is_deleted` bit(1) NULL AFTER `password`;
 insert into patch_version(patch_no,depend_patch,is_success) values(3,2,1);
 set foreign_key_checks=1;
 set sql_safe_updates=1;
